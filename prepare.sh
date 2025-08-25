dfecb32448893f29f431510c729b73585d52f549
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2025-08-25 13:26:16 +0300

    prepare repo for migration to ArgoCD repo
    Signed-off-by: Patroklos Papapetrou <ppapapetrou76@gmail.com>

---
+++ prepare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir dir1
+mv *[^dir1prepare.sh]* dir1/
+git rm -r pkg specs agent internal go.* Dockerfile LICENSE OWNERS README.md docs hack codecov.yml Makefile sonar-project.properties .github/ .golangci.yaml
+rm dir1/sonar-project.properties dir1/codecov.yml
+mv dir1/ gitops-engine
+git add gitops-engine
+git add prepare.sh
+git checkout -b migrate
+git push
